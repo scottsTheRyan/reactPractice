92e77a832e4feb59b0bf1ab92423303a89d69889
thedarkness <thedarkness@thedarknesss-MacBook-Pro.local>
2023-03-15 11:03:16 -0700

    practin' through note placeholder

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>Q Placeholder</h1>
+            <p>A Placeholder</p>
+        </div>
+    )
+}
+
+export default Note;
